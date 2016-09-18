63adcca6b6fbb0c339c1b55d722f304d5394fca6
Jason <nicholsonj863@gmail.com>
2016-09-18 14:25:20 -0400

    - Altered Javascript file in order to capture user input into text input on enter key press - Using user input, creating new span and li element under ul - Altered click method to on method so that the newly created li's from the user input had the same functionality of the other li's

--- ToDoList/app/Assets/JS/todos.js
+++ ToDoList/app/Assets/JS/todos.js
@@ -3,12 +3,13 @@
  */
 
 //check off specific todos by clicking
-$("li").click(function () {
+//using on method, using parent element and then specifying li in passed parameters
+$("ul").on("click", "li", function () {
   $(this).toggleClass("completed");
 });
 
 //click on X to delete todo
-$("span").click(function (event) {
+$("ul").on("click", "span", function (event) {
   //use parent method to remove immediate parent element
   //fadeout has call back function that will remove list item after fadeout has run
   $(this).parent().fadeOut(500, function () {
@@ -17,4 +18,15 @@ $("span").click(function (event) {
   });
   //stops click listener from bubbling up into parent elements
   event.stopPropagation();
+});
+
+$("input[type = text]").keypress(function () {
+  //using the which command to check which key is being pressed
+  //checking if the enter key is pressed (13) and capturing user input
+  if(event.which === 13){
+    var todoText = $(this).val();
+    $(this).val("");
+    //create new li and add to ul
+    $("ul").append("<li><span>X</span>" + todoText + "</li>")
+  }
 });
